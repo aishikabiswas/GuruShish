f7d479eb5857a5848bedf8b7e1d99adeb2c4cb70
Sailaja Adapa <adapasailaja17@gmail.com>
2025-06-07 17:35:09 +0530

    updated module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,11 +6,11 @@ import { join } from 'path';
 import { UserModule } from './user/user.module';
 import { User } from './user/user.entity';
 
-import { TeacherModule } from './teacher/teacher.module';
-import { Teacher } from './teacher/entities/teacher.entity';
-
 import { TeacherProfileModule } from './teacher-profile/teacher-profile.module';
 import { TeacherProfile } from './teacher-profile/entities/teacher-profile.entity';
+
+import { TeacherModule } from './teacher/teacher.module';
+import { Teacher } from './teacher/entities/teacher.entity';
 
 import { BookingsModule } from './bookings/bookings.module';
 import { Booking } from './bookings/bookings.entity';
@@ -18,10 +18,10 @@ import { Booking } from './bookings/bookings.entity';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'uploads'),
-      serveRoot: '/uploads',
+      rootPath: join(__dirname, '..', 'uploads'), // Serve static files from uploads folder
+      serveRoot: '/uploads', // URL prefix
     }),
-     TypeOrmModule.forRoot({
+    TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'dpg-d0rg2b15pdvs73dv7gc0-a',
       port: 5432,
@@ -29,12 +29,13 @@ import { Booking } from './bookings/bookings.entity';
       password: '9xYqe4jZuekeurKBnNkYctrC5NXDa0GJ',
       database: 'gurushish',
       entities: [User, Teacher, TeacherProfile, Booking],
-      synchronize: true, // Disable in production
+      synchronize: true, 
     }),
+
     UserModule,
     TeacherModule,
     TeacherProfileModule,
-    BookingsModule,
+    BookingsModule,  // import booking module here
   ],
 })
 export class AppModule {}
