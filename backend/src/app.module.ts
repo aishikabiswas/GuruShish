6a59e2c894fbb2381f32689ecf8587ec24a1d25a
Sailaja Adapa <adapasailaja17@gmail.com>
2025-07-23 21:56:18 +0530

    Updated

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,19 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { ConfigModule } from '@nestjs/config';
-
 import { UserModule } from './user/user.module';
 import { User } from './user/user.entity';
-
 import { TeacherProfileModule } from './teacher-profile/teacher-profile.module';
 import { TeacherProfile } from './teacher-profile/entities/teacher-profile.entity';
-
 import { TeacherModule } from './teacher/teacher.module';
 import { Teacher } from './teacher/entities/teacher.entity';
-
 import { BookingsModule } from './bookings/bookings.module';
 import { Booking } from './bookings/bookings.entity';
-
 @Module({
   imports: [
     ConfigModule.forRoot({
