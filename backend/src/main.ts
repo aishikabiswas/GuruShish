22e058335931a468b95c2162d13c0ce3e2660666
Sailaja Adapa <adapasailaja17@gmail.com>
2025-07-23 22:14:17 +0530

    Updated

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,15 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://guru-shish-dd3x.vercel.app',
-    ],
+    origin: ['http://localhost:3000', 'https://guru-shish-dd3x.vercel.app'],
     credentials: true,
   });
 
-  const port = process.env.PORT || 3043;
-  await app.listen(port);
-  console.log(`Backend running on http://localhost:${port}`);
+  await app.listen(process.env.PORT || 3043);
 }
 bootstrap();
