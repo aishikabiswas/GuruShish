6d5e4b5607bc662cc78ca39980b1d120d1e4a32b
Sailaja Adapa <adapasailaja17@gmail.com>
2025-07-24 20:56:39 +0530

    Updated Backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -28,7 +28,7 @@ import { Booking } from './bookings/bookings.entity';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10),
+      port: 5432,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
